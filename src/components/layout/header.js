0ee924da715e2dc09db9f8434b12fd67b5bc4239
Gramli <Gramli@seznam.cz>
2021-05-24 22:56:18 +0200

    start implementation

---
+++ src/components/layout/header.js
@@ -0,0 +1,19 @@
+import styles from './header.module.css';
+import mealsPicture from '../../assets/meals.jpg';
+import HeaderCartButton from './header-cart-button';
+
+const Header = (props) => {
+    return (
+        <>
+        <header className={styles.header}>
+            <h1>React Guide Meals Order</h1>
+            <HeaderCartButton>Cart</HeaderCartButton>
+        </header>
+        <div className={styles["main-image"]}>
+            <img src={mealsPicture} alt="food table"/>
+        </div>
+        </>
+    );
+}
+
+export default Header;
